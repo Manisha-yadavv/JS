b4ff973b8dc8fb277e0cde4cac69e53989ec67b7
Manisha-yadavv <139050643+Manisha-yadavv@users.noreply.github.com>
2024-02-03 19:46:23 +0000

    config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all Js code as newer version
+
+// alert(3+3) // WE ARE USING NODE JS NOT BROWSER
+
+// CODE READABILITY SHOULD BE HIGH
+// MDN DOCUMENETATION IS NOT ORIGINAL DOCUMENTATION OF JAVASCRIPT, tc39.es ECMAScript is 
+
+// PRIMITIVE DATA TYPES
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// undefined => value is not assigned yet
+// null => standalone value
+// symbol => symbol
+
+// objects
+
+let name="Manisha"
+let age= 24
+
+console.log(typeof "Manisha"); 
+console.log(typeof age);
+console.log(typeof null); // type OBJECT
+console.log(typeof undefined); // type UNDEFINED
+
+// ALL DATATYPES ARE TYPES OF THEIR OWN LIKE STRING, UNDEFINED ETC BUT NULL DATATYPE IS ACTUALLY AN OBJECT, ITS NOT TYPE OF ITS OWN
