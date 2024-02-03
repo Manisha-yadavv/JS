0e727a225e3c99155d0002a218796aea2694bb01
Manisha-yadavv <139050643+Manisha-yadavv@users.noreply.github.com>
2024-02-03 20:35:44 +0000

    config done

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,22 @@
+//let score = 33
+// console.log(typeof score)
+
+// TO NUMBER
+
+/*let score="33"          (STRING TO NUMBER)
+let scoreInNumber = Number(score)
+console.log(typeof scoreInNumber) */
+
+let isAvailable = true
+let inNumber = Number(isAvailable)
+console.log(typeof inNumber)
+console.log(inNumber)
+
+let isThere=""
+let isHere= "Manisha"
+let wasThere= Number(isThere)
+let wasHere= Number(isHere)
+console.log(typeof wasThere)
+console.log(typeof wasHere)
+console.log(wasThere)
+console.log(wasHere)
