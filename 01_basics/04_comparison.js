13cc2d12152fcfe1f281d4ccdaa608ea7099758f
Manisha-yadavv <139050643+Manisha-yadavv@users.noreply.github.com>
2024-02-04 13:35:03 +0000

    config done

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1)
+// console.log(2>=1)
+// console.log(2 < 1)
+// console.log(2<=1)
+// console.log(2==1)
+// console.log(2!=1)
+
+// console.log("2" > 1)
+
+// console.log(null > 0)     (comparison operator)
+// console.log(null >= 0)     (comparison operator)
+// console.log(null == 0)     (equality check operator)
+// EQUALITY CHECK AND COMPARISONS WORK DIFFERENTLY. 
+// COMPARSISONS CONVERT NULL TO A NUMBER, TREATING IT AS 0.
+
+// console.log(undefined > 0)
+// console.log(undefined >= 0)
+// console.log(undefined == 0)
+// AVOID THESE TYPE OF CONVERSIONS
+
+
+// strict check ===
+console.log("2" === 2)
