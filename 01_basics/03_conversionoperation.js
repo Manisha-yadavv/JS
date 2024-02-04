1b951ce4c057880b59c7aaa1000708f055c6416a
Manisha-yadavv <139050643+Manisha-yadavv@users.noreply.github.com>
2024-02-04 09:44:52 +0000

    config done

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,22 +1,80 @@
-//let score = 33
-// console.log(typeof score)
+// ANY OTHER DATATYPE TO NUMBER
 
-// TO NUMBER
+/*let score = 33
+console.log(typeof score)
+console.log(score)
 
-/*let score="33"          (STRING TO NUMBER)
-let scoreInNumber = Number(score)
-console.log(typeof scoreInNumber) */
+let marks = "33"
+console.log(typeof marks)
+console.log(marks)
+let marksInNumber = Number(marks)
+console.log(typeof marksInNumber)
+console.log(marksInNumber)
 
-let isAvailable = true
-let inNumber = Number(isAvailable)
-console.log(typeof inNumber)
-console.log(inNumber)
+let run = "33abc"
+console.log(typeof run)
+console.log(run)
+let runInNum = Number(run)
+console.log(typeof runInNum)  it is converted into number But
+console.log(runInNum)     value is not a number, NaN 
 
-let isThere=""
-let isHere= "Manisha"
-let wasThere= Number(isThere)
-let wasHere= Number(isHere)
-console.log(typeof wasThere)
-console.log(typeof wasHere)
-console.log(wasThere)
-console.log(wasHere)
+let run = null
+console.log(typeof run)
+console.log(run)
+let runInNum = Number(run)
+console.log(typeof runInNum)
+console.log(runInNum)    WHEN WE CONVERT NULL TO NUMBER IT GIVES VALUE 0 
+
+let run = undefined
+console.log(typeof run)
+console.log(run)
+let runInNum = Number(run)
+console.log(typeof runInNum)
+console.log(runInNum)   WHEN WE CONVERT UNDEFINED TO NUMBER IT GIVES VALUE NaN    
+
+let score = true
+console.log(typeof score)
+console.log(score)
+let scoreInNum = Number(score)
+console.log(typeof scoreInNum)
+console.log(scoreInNum)  BOOLEAN(TRUE) TO NUMBER, WE GET 1   
+
+let score = false
+console.log(typeof score)
+console.log(score)
+let scoreInNum = Number(score)
+console.log(typeof scoreInNum)
+console.log(scoreInNum)    Boolean(FALSE) TO NUMBER WE GET 0   
+
+ ANY OTHER DATATYPE TO BOOLEAN 
+
+ let score = 1
+ console.log(typeof score)
+ console.log(score)
+ let isScore = Boolean(score)
+ console.log(typeof isScore)
+ console.log(isScore)    WE GET TRUE
+
+ let score = 0
+ console.log(typeof score)
+ console.log(score)
+ let isScore = Boolean(score)
+ console.log(typeof isScore)
+ console.log(isScore)     WE GET FALSE               
+
+ let score = "manisha"
+ console.log(typeof score)
+ console.log(score)
+ let isScore = Boolean(score)
+ console.log(typeof isScore)
+ console.log(isScore)  STRING TO BOOLEAN WE GET TRUE  
+
+ let score = ""
+ console.log(typeof score)
+ console.log(score)
+ let isScore = Boolean(score)
+ console.log(typeof isScore)
+ console.log(isScore)   "" TO BOOLEAN WE GET FALSE 
+
+
+
