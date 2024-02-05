a8f8710b9b5a72d861f3998eb00fe71c4badd043
Manisha-yadavv <139050643+Manisha-yadavv@users.noreply.github.com>
2024-02-05 18:06:19 +0000

    container config done

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -3,5 +3,22 @@
 // console.log(name + language)     DON'T DO THIS, THIS SYNTAX IS OUTDATED
 // console.log(`Hi, my name is ${name} and i am learning ${language}`)  THIS IS MODERN WAY (INTERPOLATION), THIS IS MORE READABLE
 
-const gameName = new String("Manisha")
-console.log(gameName)
+//const gameName = new String("Manisha-yadav-sunflower")  CAN'T REMOVE WHITESPACES USING TRIM IN THIS STRING
+// console.log(gameName)
+// console.log(gameName.length)
+// console.log(gameName.charAt(2))
+// console.log(gameName.indexOf('n'))
+// console.log(gameName.lastIndexOf('a'))
+// const name = gameName.substring(1,6)   YOU CANNOT GIVE NEGATIVE VALUES IN THIS METHOD BUT YOU CAN IN SLICE METHOD
+// console.log(name)
+// const name = gameName.slice(-7,6)
+//  console.log(name)
+//  const newName = "    Manisha   "    CAN REMOVE WHITESPACES USING TRIM METHOD IN THIS STRING
+//  console.log(newName)
+//  console.log(newName.trim())
+
+//  const url = "man%20isha%20yadav%20@gmail.com"
+//  console.log(url.replace('%20','-'))
+//  console.log(url.includes('an'))
+
+console.log(gameName.split('-'))  // TO CONVERT STRING INTO ARRAY
