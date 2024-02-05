191f13b61f405fcb74ea62ff0407e8c63d0ed573
Manisha-yadavv <139050643+Manisha-yadavv@users.noreply.github.com>
2024-02-05 17:13:21 +0000

    container config done

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,7 @@
+// const name = "Manisha"
+// const language = "Javascript"
+// console.log(name + language)     DON'T DO THIS, THIS SYNTAX IS OUTDATED
+// console.log(`Hi, my name is ${name} and i am learning ${language}`)  THIS IS MODERN WAY (INTERPOLATION), THIS IS MORE READABLE
+
+const gameName = new String("Manisha")
+console.log(gameName)
