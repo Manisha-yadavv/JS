ddeaaca4daeb1d42aef04b0e2a10a8820b8a9069
Manisha-yadavv <139050643+Manisha-yadavv@users.noreply.github.com>
2024-02-05 19:07:23 +0000

    container config done

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,7 +1,13 @@
-const num = 200
-console.log(num)
+// const num = 200
+// console.log(num)
 
-const num1 = new Number(200)
-console.log(num1)
-console.log(num.toString().length)
-console.log(num.toFixed())
+// const num1 = new Number(200)
+// console.log(num1)
+// console.log(num.toString().length)
+// console.log(num.toFixed(2))
+
+// const num = 200.16
+// console.log(num.toPrecision(4))
+
+const hund = 1000000
+console.log(hund.toLocaleString('en-IN'))
